9125fce64b39be695eae3566c589580a13a1ae3b
nphto <nphto>
2018-01-20 13:26:53 -0500

    updated to the correct motors, added the solenoid variable

--- src/src/org/gosparx/team1126/subsytems/Drives.java
+++ src/src/org/gosparx/team1126/subsytems/Drives.java
@@ -1,7 +1,6 @@
 package src.org.gosparx.team1126.subsytems;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.Encoder;
 import src.org.gosparx.team1126.util.DebuggerResult;
 
@@ -9,17 +8,17 @@ public class Drives extends GenericSubsytem {
 	
 	//-----------------------------------------------------Motors/Sensors--------------------------------------------------------
 	
-	private TalonSRX rightMtr1;
+	private WPI_TalonSRX rightMtr1;
 	
-	private TalonSRX rightMtr2;
+	private WPI_TalonSRX rightMtr2;
 	
-	private TalonSRX rightMtr3;
+	private WPI_TalonSRX rightMtr3;
 	
-	private TalonSRX leftMtr1;
+	private WPI_TalonSRX leftMtr1;
 	
-	private TalonSRX leftMtr2;
+	private WPI_TalonSRX leftMtr2;
 	
-	private TalonSRX leftMtr3;
+	private WPI_TalonSRX leftMtr3;
 	
 	private Encoder rightEnc;
 	
@@ -32,12 +31,12 @@ public class Drives extends GenericSubsytem {
 	 * initializes all variables
 	 */
 	public void init() {
-		rightMtr1 = new TalonSRX(0);
-		rightMtr2 = new TalonSRX(0);
-		rightMtr3 = new TalonSRX(0);
-		leftMtr1 = new TalonSRX(0);
-		leftMtr1 = new TalonSRX(0);
-		leftMtr1 = new TalonSRX(0);
+		rightMtr1 = new WPI_TalonSRX(0);
+		rightMtr2 = new WPI_TalonSRX(0);
+		rightMtr3 = new WPI_TalonSRX(0);
+		leftMtr1 = new WPI_TalonSRX(0);
+		leftMtr1 = new WPI_TalonSRX(0);
+		leftMtr1 = new WPI_TalonSRX(0);
 		rightEnc = new Encoder(0, 0);
 		leftEnc = new Encoder(0, 0);
 	}
