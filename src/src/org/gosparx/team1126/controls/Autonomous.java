ef885936e0074bba905535d2fea13150fd923812
kevinbates <kevinbates@webster-104-guest.monroe.edu>
2018-01-20 15:11:53 -0500

    Added code to check field condition based on string given to us by field

--- src/src/org/gosparx/team1126/controls/Autonomous.java
+++ src/src/org/gosparx/team1126/controls/Autonomous.java
@@ -14,12 +14,34 @@ public class Autonomous extends Controls {
 	
 	public Autonomous() {
 		
+		isRightAllySwitch = false;
+		isRightScale = false;
+		isRightOppenentSwitch = false;
+		
 		autoChooser = new SendableChooser();
 		autoChooser.addDefault("Do Nothing", new Integer(0));
-		autoChooser.addDefault("Example1", new Integer(1));
+		autoChooser.addObject("Example1", new Integer(1));
 		//Use these to create the default and buttons that are to be selected
-		
+
 		//DriverStation.getInstance().getGameSpecificMessage(); //-> 3 character string
+	}
+	
+	public boolean setFieldConditions() {
+		String fieldConditions = DriverStation.getInstance().getGameSpecificMessage();
+		if(!fieldConditions.equals("")) {
+			if (fieldConditions.charAt(0) == 'r') {
+				isRightAllySwitch = true;
+			}
+			if (fieldConditions.charAt(1) == 'r') {
+				isRightScale = true;
+			}
+			if (fieldConditions.charAt(2) == 'r') {
+				isRightOppenentSwitch = true;
+			}
+			return true;
+		}
+		return false;
+
 	}
 	
 }
