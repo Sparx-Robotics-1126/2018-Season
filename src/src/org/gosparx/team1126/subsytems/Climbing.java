0f6310614b3a74a89a5d080293123c07ff74bc86
Justin1205 <Justin1205@10.94.38.185>
2018-01-20 14:09:18 -0500

    Added framework for climbing

--- src/src/org/gosparx/team1126/subsytems/Climbing.java
+++ src/src/org/gosparx/team1126/subsytems/Climbing.java
@@ -3,10 +3,41 @@
  */
 package src.org.gosparx.team1126.subsytems;
 
+import src.org.gosparx.team1126.util.DebuggerResult;
+
 /**
  * @author Justin1205
  *
  */
-public class Climbing {
+public class Climbing extends GenericSubsytem
+{
+
+	@Override
+	public void execute() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void logger() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DebuggerResult[] debug() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
