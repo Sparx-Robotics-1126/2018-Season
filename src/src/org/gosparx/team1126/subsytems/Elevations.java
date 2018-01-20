e1c59a4676ca7d323cbb3a978368d6f1eae3c245
Skyler <skylerssparxemail@gmail.com>
2018-01-20 11:23:01 -0500

    Added Framework

---
+++ src/src/org/gosparx/team1126/subsytems/Elevations.java
@@ -0,0 +1,32 @@
+package src.org.gosparx.team1126.subsytems;
+
+import src.org.gosparx.team1126.util.DebuggerResult;
+
+public class Elevations extends GenericSubsytem {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void logger() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DebuggerResult[] debug() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
