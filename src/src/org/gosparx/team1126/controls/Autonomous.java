4245dc0d7b1d3d369f665bcb0b859414bd460709
kevinbates <kevinbates@webster-104-guest.monroe.edu>
2018-01-20 12:05:10 -0500

    added teleOp class

--- src/src/org/gosparx/team1126/controls/Autonomous.java
+++ src/src/org/gosparx/team1126/controls/Autonomous.java
@@ -1,5 +1,11 @@
 package src.org.gosparx.team1126.controls;
 
+import edu.wpi.first.wpilibj.DriverStation;
+
 public class Autonomous extends Controls {
 
+	public Autonomous() {
+		//DriverStation.getInstance().getGameSpecificMessage(); //-> 3 character string
+	}
+	
 }
