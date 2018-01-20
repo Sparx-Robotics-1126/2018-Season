1a6d361627abdc4c63573f4f347469157e40490c
nphto <nphto>
2018-01-20 12:15:26 -0500

    Created the CIM and Encoder variables

--- src/src/org/gosparx/team1126/subsytems/Drives.java
+++ src/src/org/gosparx/team1126/subsytems/Drives.java
@@ -1,18 +1,50 @@
 package src.org.gosparx.team1126.subsytems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Encoder;
 import src.org.gosparx.team1126.util.DebuggerResult;
 
 public class Drives extends GenericSubsytem {
+	
+	//-----------------------------------------------------Motors/Sensors--------------------------------------------------------
+	
+	private TalonSRX rightMtr1;
+	
+	private TalonSRX rightMtr2;
+	
+	private TalonSRX rightMtr3;
+	
+	private TalonSRX leftMtr1;
+	
+	private TalonSRX leftMtr2;
+	
+	private TalonSRX leftMtr3;
+	
+	private Encoder rightEnc;
+	
+	private Encoder leftEnc;
+	
+	//---------------------------------------------------------Code--------------------------------------------------------------
 
 	@Override
+	/**
+	 * initializes all variables
+	 */
 	public void init() {
-		// TODO Auto-generated method stub
-		
+		rightMtr1 = new TalonSRX(0);
+		rightMtr2 = new TalonSRX(0);
+		rightMtr3 = new TalonSRX(0);
+		leftMtr1 = new TalonSRX(0);
+		leftMtr1 = new TalonSRX(0);
+		leftMtr1 = new TalonSRX(0);
+		rightEnc = new Encoder(0, 0);
+		leftEnc = new Encoder(0, 0);
 	}
 	
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
