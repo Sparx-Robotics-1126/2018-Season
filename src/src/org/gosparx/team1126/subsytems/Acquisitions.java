909ecce21c940892a09adf3b12952722eb05ea5a
chees <cheesemanaidan@gmail.com>
2018-01-20 11:30:05 -0500

    Added acquisitions framework

---
+++ src/src/org/gosparx/team1126/subsytems/Acquisitions.java
@@ -0,0 +1,31 @@
+package src.org.gosparx.team1126.subsytems;
+
+import src.org.gosparx.team1126.util.DebuggerResult;
+
+public class Acquisitions extends GenericSubsytem{
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void logger() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DebuggerResult[] debug() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
