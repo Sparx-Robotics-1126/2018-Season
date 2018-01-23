4a5d0b257211425d68d9e3c5c6ceac73c6154cd7
kevinbates <kevinbates@webster-104-guest.monroe.edu>
2018-01-22 20:04:37 -0500

    added example states and runs for auto

--- src/src/org/gosparx/team1126/controls/Autonomous.java
+++ src/src/org/gosparx/team1126/controls/Autonomous.java
@@ -9,22 +9,40 @@ public class Autonomous extends Controls {
 	private boolean isRightScale;
 	private boolean isRightOpponentSwitch;
 	
-	SendableChooser autoChooser;
+	private AutoSelected selectedAuto;
 	
-	private int selectedAuto;
+	private boolean firstRun;
+	
+	private SendableChooser<AutoSelected> autoChooser;
+	
+	//private int selectedAuto;
 	//used to hold a reference to a sendableChooser Object
 	
+	private int[][] currentAuto;
+	
+	private final int[][] TESTAUTO = {
+		{stateToInt(AutoState.DRIVES_TURNLEFT), 45, 10},
+		{stateToInt(AutoState.DRIVES_FORWARD), 10, 10},
+		{stateToInt(AutoState.DRIVES_TURNRIGHT), 45, 10},
+		{stateToInt(AutoState.DRIVES_FORWARD), 10, 10}
+	};
+	
 	public Autonomous() {
+		
+		firstRun = false;
 		
 		isRightAllySwitch = false;
 		isRightScale = false;
 		isRightOpponentSwitch = false;
 		
-		autoChooser = new SendableChooser();
-		autoChooser.addDefault("Do Nothing", new Integer(0));
-		autoChooser.addObject("Example1", new Integer(1));
+		autoChooser = new SendableChooser<AutoSelected>();
+		//test
+		autoChooser.addDefault("Do Nothing", selectedAuto = AutoSelected.NOTHING);
+		autoChooser.addObject("Cross The Border", selectedAuto = AutoSelected.CROSSBORDER);
+		autoChooser.addObject("Score The Scale", selectedAuto = AutoSelected.SCALE);
+		autoChooser.addObject("Score The Switch", selectedAuto = AutoSelected.SWITCH);
 		//Use these to create the default and buttons that are to be selected
-		selectedAuto = ((Integer) autoChooser.getSelected()).intValue();
+		
 		
 
 		//DriverStation.getInstance().getGameSpecificMessage(); //-> 3 character string
@@ -61,18 +79,60 @@ public class Autonomous extends Controls {
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		
-		
 		if(isAutonomous() && isEnabled()) {
-			
+			if(!firstRun) {
+				firstRun = true;
+				switch(selectedAuto) {
+				case NOTHING:
+				case SCALE:
+				case CROSSBORDER:
+				case SWITCH:
+				}
+			}
 			
 			
 		} else {
-			selectedAuto = ((Integer) autoChooser.getSelected()).intValue();
+			
 		}
 		
 		
 	}
+	
+	public enum AutoState{
+		
+		DRIVES_FORWARD, //@param - distance, speed
+		DRIVES_BACKWARD, //@param - distance, speed
+		DRIVES_TURNLEFT, //@param - degrees to turn, speed
+		DRIVES_TURNRIGHT, //@param - degrees to turn, speed
+		DRIVES_WAIT;
+		
+	}
+	
+	public int stateToInt(AutoState auto) {
+		switch(auto) {
+		case DRIVES_FORWARD:
+			return 1;
+		case DRIVES_BACKWARD:
+			return 2;
+		case DRIVES_TURNLEFT:
+			return 3;
+		case DRIVES_TURNRIGHT:
+			return 4;
+		case DRIVES_WAIT:
+			return 5;
+		default:
+			return -999;
+		}
+	}
+	
+	public enum AutoSelected{
+		
+		NOTHING,
+		SCALE,
+		CROSSBORDER,
+		SWITCH;
+		
+	}
+	
 	
 }
