17e7609cb354d6b6790375e6b5d9ad790a65b2e0
secretspy97 <nathan.connor@gosparx.org>
2018-01-25 18:37:29 -0500

    Fixed build path

--- src/src/org/gosparx/team1126/subsytems/GenericSubsytem.java
+++ src/src/org/gosparx/team1126/subsytems/GenericSubsytem.java
@@ -2,14 +2,43 @@ package src.org.gosparx.team1126.subsytems;
 
 import src.org.gosparx.team1126.util.DebuggerResult;
 
-public abstract class GenericSubsytem {
+public abstract class GenericSubsytem extends Thread{
 
-	public abstract void execute();
+	private String name;
+	public GenericSubsytem(String name){
+		this.name = name;
+		setPriority(Thread.NORM_PRIORITY);
+	}
 	
 	public abstract void init();
 	
-	public abstract void logger();
+	public abstract void execute();
+	
+	public abstract void forceStandby();
+	
+	public abstract boolean isDone();
+	
+	public abstract long sleepTime();
 	
 	public abstract DebuggerResult[] debug();
 	
+	protected void log(String message){
+		print(message);
+	}
+	
+	protected void print(String message){
+		System.out.println(name + ": " + message);
+	}
+	
+	@Override
+	public void run(){
+		log("Initializing " + name + "...");
+		init();
+		long timeWait = sleepTime();
+		log("Starting " + name + "...");
+		while(true){
+			execute();
+			try { Thread.sleep(timeWait); } catch (InterruptedException e) {}
+		}
+	}
 }
