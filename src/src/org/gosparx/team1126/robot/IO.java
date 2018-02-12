75ef03e302039f065e72b0baa769f9554bf0ad9c
Justin1205 <Justin1205@10.94.38.185>
2018-02-12 18:43:17 -0500

    Made a solenoid

--- src/src/org/gosparx/team1126/robot/IO.java
+++ src/src/org/gosparx/team1126/robot/IO.java
@@ -1,5 +1,5 @@
 package src.org.gosparx.team1126.robot;
 
 public class IO {
-
+	public static final int ptoSwitch = 7;
 }
--- src/src/org/gosparx/team1126/subsytems/Climbing.java
+++ src/src/org/gosparx/team1126/subsytems/Climbing.java
@@ -3,15 +3,19 @@
  */
 package src.org.gosparx.team1126.subsytems;
 
+import edu.wpi.first.wpilibj.Solenoid;
+import src.org.gosparx.team1126.robot.IO;
 import src.org.gosparx.team1126.util.DebuggerResult;
-
 /**
  * @author Justin1205
  *
  */
 public class Climbing extends GenericSubsytem
 {
-
+	Solenoid climbingSoul;
+	
+	public State climbingState;
+	
 	public Climbing() {
 		super("Climbing");
 	}
@@ -20,14 +24,27 @@ public class Climbing extends GenericSubsytem
 	public void execute() 
 	{
 		// TODO Auto-generated method stub
+		switch (climbingState)
+		{
+		case active:
+			climbingSoul.set(true);
+			break;
+		case inactive:
+			climbingSoul.set(false);
+			break;
 		
+		default:
+		    System.out.println("You done messed up ~Ghandi");
+			break;
+		
+		}
 	}
 
 	@Override
 	public void init() 
 	{
 		// TODO Auto-generated method stub
-		
+		climbingSoul = new Solenoid(IO.ptoSwitch);
 	}
 
 	@Override
@@ -38,26 +55,17 @@ public class Climbing extends GenericSubsytem
 	}
 	public enum State
 	{
-	STANDBY,
-	ATTACHING,
-	CLIMBING,
-	HELPINGHANDS;
+	active,
+	inactive;
 		
 		@Override
 		public String toString(){
 			switch(this)
 			{
-			case STANDBY:
-				return "Climbing standby";
-				
-			case ATTACHING:
-				return "Climber attaching";
-				
-			case CLIMBING:
-				return "Climbing";
-				
-			case HELPINGHANDS:
-				return "Deploying helping hands";
+			case active:
+				return "Climbing active";
+			case inactive:
+				return "Climbing deactivated";
 			}
 			return null;
 		}
